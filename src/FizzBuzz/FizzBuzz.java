9a80aafc958347f9463ac272bfbf5d5c02ffa311
Foxtails-design <littlefither@gmail.com>
2019-10-04 23:17:21 +0700

    Phase Fail
    Change Test 1 + 2  ให้ทำงานเหมือนกัน
    และเพิ่ม Before each เพื่อ เซตค่า fizzbuzz ใหม่ก่อน test ทุกเคส

--- src/FizzBuzz/FizzBuzz.java
+++ src/FizzBuzz/FizzBuzz.java
@@ -1,17 +1,32 @@
 package FizzBuzz;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+
 
 class FizzBuzz {
 	
-	FizzBuzzTest fizzBuzz = new FizzBuzzTest();
-	String output = fizzBuzz.getBuzz(1);
-
-	@Test
-	void test_1() {
-		assertEquals("Buzz", output);
+	FizzBuzzTest fizzBuzz;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		fizzBuzz =  new FizzBuzzTest();
 	}
-
+	
+	@DisplayName("Actual1")
+	@Test
+	void ShouldBe1_Input1() {
+		String output = fizzBuzz.getBuzz(1);
+		assertEquals(1, output);
+	}
+	
+	@DisplayName("Actual2")
+	@Test
+	void ShouldBe2_Input2() {
+		String output = fizzBuzz.getBuzz(2);
+		assertEquals(2, output);
+	}
+	
 }
